95fd4baa748a004dad200e1a7e5bd4c0758b01d0
Артур Бутюгов <77003005+arthur810629@users.noreply.github.com>
2024-03-18 21:15:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function nameAge(name, age) {
+    if (age < 18) {
+        return "Дорогой(ая) " + name + ", ты слишком молод(а)!";
+    } else {
+        return "Привет, " + name + "!";
+    }
+}
+
+nameAge("Артур", 19)
+
+let age = 10
+let name = "Артур"
